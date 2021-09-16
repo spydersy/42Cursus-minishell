b6eee84f8abc7be6d07294b2d3ef295d5d4fdb85
spydersy <acbe1325@aol.com>
2021-09-16 15:38:39 +0100

    builtins(cd, env, pwd, exit, echo, unset) : DONE | TODO : builtin(export), get_exec_path must not return NULL

--- builtins/buil_export.c
+++ builtins/buil_export.c
@@ -6,71 +6,71 @@
 /*   By: abelarif <abelarif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/17 16:18:08 by abelarif          #+#    #+#             */
-/*   Updated: 2021/09/10 12:19:47 by abelarif         ###   ########.fr       */
+/*   Updated: 2021/09/16 14:17:34 by abelarif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-// void        print_export(void)
-// {
-//     int     i;
+void        print_export(void)
+{
+    int     i;
 
-//     i = -1;
-//     while (g_env.sorted[++i])
-//     {
-//         ft_putstr_fd("declare -x ", 1);
-//         ft_putstr_fd(g_env.sorted[i], 1);
-//     }
-// }
+    i = -1;
+    while (g_env.sorted[++i])
+    {
+        ft_putstr_fd("declare -x ", 1);
+        ft_putstr_fd(g_env.sorted[i], 1);
+    }
+}
 
-// int export_checker(char **arg, int index)
-// {
-//     if ((arg[index][0] == ' ' || arg[0] == '\t') 
-//         & arg[index][1] == '=')
-//     {
-//         return (index);
-//     }
-//     return (-1);
-// }
+int export_checker(char **arg, int *types, int index)
+{
+    if ((arg[index][0] == ' ' || arg[0] == '\t') 
+        & arg[index][1] == '=')
+    {
+        return (index);
+    }
+    return (-1);
+}
 
-// void    export_error(char **args, int index)
-// {
-//     ft_putstr_fd(KRED, STDERR);
-//     ft_putstr_fd("export: `", STDERR);
-//     ft_putstr_fd(args[index], STDERR);
-//     ft_putendl_fd("': not a valid identifier", STDERR);    
-// }
+void    export_error(char **args, int index)
+{
+    ft_putstr_fd(KRED, STDERR);
+    ft_putstr_fd("export: `", STDERR);
+    ft_putstr_fd(args[index], STDERR);
+    ft_putendl_fd("': not a valid identifier", STDERR);    
+}
 
-// void    export_args(char **args)
-// {
-//     int     i;
+void    export_args(char **args, int *types)
+{
+    int     i;
 
-//     i  -1;
-//     while (args[++i])
-//     {
-//         if (export_checker(args, i) == -1)
-//         {
-//             push_
-//         }
-//         else
-//         {
-//             export_error(args, i);
-//         }
-//     }    
-// }
+    i  -1;
+    while (args[++i])
+    {
+        if (export_checker(args, types, i) == -1)
+        {
 
-// void        builtin_export(char **args)
-// {
-//     int     count;
-//     int     i;
+        }
+        else
+        {
+            export_error(args, i);
+        }
+    }    
+}
 
-//     i = -1;
-//     count = 0;
-//     while (args[count])
-//         count++;
-//     if (count == 0)
-//         print_export();
-//     else
-//         export_args(args);
-// }
+void        builtin_export(char **args, int *types)
+{
+    int     count;
+    int     i;
+
+    i = -1;
+    count = 0;
+    while (args[count])
+        count++;
+    if (count == 0)
+        print_export();
+    else
+        export_args(args, types);
+}
